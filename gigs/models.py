d655df6c22a36327f5464167bca4550dbf0b8eaf
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-31 14:01:53 +0100

    added profile task model

--- gigs/models.py
+++ gigs/models.py
@@ -11,6 +11,8 @@ class Profile(User):
     Female = 'F'
     GENDER = [ (Male,'Male'), (Female, 'Female')]
     gender = models.CharField(choices=GENDER, max_length=1)
+    wallet_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    earnings = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     def __str__(self):
         return self.user.username
@@ -32,10 +34,29 @@ class Task(models.Model):
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.CharField(max_length=100)
-    sample_image = models.ImageField(upload_to='images/')
+    sample_image = models.ImageField(upload_to='media/')
 
     def __str__(self):
         return self.title
+    
+class ProfileTask(models.Model):
+    PENDING = 'PENDING'
+    APPROVED = 'APPROVED'
+    REJECTED = 'REJECTED'
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (APPROVED, 'Approved'),
+        (REJECTED, 'Rejected')
+    ]
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=PENDING)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.profile.user.username + ' - ' + self.task.title
 
 
 
