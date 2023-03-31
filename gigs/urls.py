795be42986dc8b724a0e8d57c5f8365f69d955d6
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-31 11:03:24 +0100

    conig app urls

--- gigs/urls.py
+++ gigs/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('jobs/', views.jobs, name='jobs'),
+    path('post_job/', views.post_job, name='post_job'),
+    path('post_job_new/', views.post_job_new, name='post_job_new'),
+    path('profile/', views.profile, name='profile'),
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
