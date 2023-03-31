85f0330b91f55a2fa9954ac63e6d91c7f68ebc9f
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-31 10:49:45 +0100

    added profile fom

--- gigs/forms.py
+++ gigs/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 
 from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
 
-class ProfileForm(forms.ModelForm):
+class ProfileForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters only.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required. 30 characters or fewer. Letters only.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
+
     class Meta:
         model = Profile
-        fields = '__all__'
-        widgets = {
-            'gender': forms.RadioSelect()
-        }
+        fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2', 'gender']
