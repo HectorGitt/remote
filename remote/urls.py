5318a644557ac0bcd6fb9e82d8ea562fb5ed149d
Hector <adeniyi.olaitanhector@yahoo.com>
2023-03-31 11:02:14 +0100

    setup django reload and tinymce

--- remote/urls.py
+++ remote/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('gigs.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
