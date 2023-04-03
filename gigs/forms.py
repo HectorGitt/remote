5874141f79cdc8da474f58c21c7caeacc37e3eb0
Hector <adeniyi.olaitanhector@yahoo.com>
2023-04-03 15:12:25 +0100

    added taskform to create task

--- gigs/forms.py
+++ gigs/forms.py
@@ -12,3 +12,12 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name','email', 'password1', 'password2', 'gender']
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'unit_price', 'total_participants','cost', 'category', 'tags', 'sample_image']
+        widgets = {
+        'unit_price':forms.NumberInput(attrs={'max':99999, 'min':1}),
+        'total_participants':forms.NumberInput(attrs={'max':999999999, 'min':1}),
+        'cost':forms.NumberInput(attrs={'max':9999999999, 'min':500.00, 'readonly':True}),
+        }
