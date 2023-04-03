63850c169f2df1267e62b2dde7b2804a7cc9e934
Hector <adeniyi.olaitanhector@yahoo.com>
2023-04-03 15:10:16 +0100

    registered models to admin

--- gigs/admin.py
+++ gigs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Category, Task
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Task)
+
