798afc9a2264799fa19636d9e32f5f23a4fb1ce5
Hector <adeniyi.olaitanhector@yahoo.com>
2023-04-09 18:37:57 +0100

    imported decorator handler for class view

--- gigs/views.py
+++ gigs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from .forms import UserForm, TaskForm
 from django.contrib.auth.decorators import login_required
 from .models import User, Category, Task, UserTask
