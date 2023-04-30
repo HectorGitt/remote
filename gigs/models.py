014d773cb1fea63d06c6603fcfce65df9dddfc5b
Hector <adeniyi.olaitanhector@yahoo.com>
2023-04-30 20:19:19 -0100

    added proof file upload

--- gigs/models.py
+++ gigs/models.py
@@ -111,6 +111,9 @@ class UserTask(models.Model):
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=PENDING)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+    image1 = models.ImageField(upload_to='media/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='media/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='media/', blank=True, null=True)
 
     def __str__(self):
         return self.user.username + ' - ' + self.task.title
