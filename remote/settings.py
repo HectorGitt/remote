d36b7ab3921fc7b45bd5895d85c149a30bcad97b
Hector <adeniyi.olaitanhector@yahoo.com>
2023-09-26 17:30:20 +0100

    fixed theme path

--- remote/settings.py
+++ remote/settings.py
@@ -217,7 +217,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    'theme/static/',
+    'theme/',
 ]
 MEDIA_ROOT=config('MEDIA_ROOT')
 MEDIA_URL=''
