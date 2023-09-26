83a571bcba9027f5f11eb5428da20cbcda86f817
Hector <adeniyi.olaitanhector@yahoo.com>
2023-09-26 18:28:35 +0100

    setup build file

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 
 pip freeze > requirements.txt
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations gigs
 python manage.py migrate
