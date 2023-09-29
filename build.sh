cdfee1c6f93acfa24287d4896aa52e949e027fa2
Hector <adeniyi.olaitanhector@yahoo.com>
2023-09-29 23:51:11 +0100

    refractor build script

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
-
+# install dependencies
 pip install -r requirements.txt
+
+# collect static files
 python manage.py collectstatic --no-input
+
+# build tailwind
 python manage.py tailwind build
+
+# run database migrations
 python manage.py makemigrations gigs
 python manage.py migrate
