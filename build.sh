1103eb9e3e768a71673ecc2638a1fe56da0dc23d
Hector <adeniyi.olaitanhector@yahoo.com>
2023-09-30 01:43:21 +0100

    updated build file

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # build tailwind
+npm install rimraf
 export NODE_OPTIONS=--openssl-legacy-provider
 npm install -D tailwindcss
 python manage.py tailwind build
