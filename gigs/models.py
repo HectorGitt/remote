411929e6dfb93e15dd883a63956bb4fe0dae0833
Hector <adeniyi.olaitanhector@yahoo.com>
2023-10-03 01:05:03 +0100

    deleted percentage_fee field

--- gigs/models.py
+++ gigs/models.py
@@ -51,7 +51,6 @@ class SubCategory(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     min_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    percentage_fee = models.DecimalField(max_digits=10, decimal_places=2, default=15.00)
 
     def __str__(self):
         return self.name + " - " + self.category.name
