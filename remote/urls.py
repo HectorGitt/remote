a8aa6bf99be33f41327073f1b6aa673addb03eb6
Hector <adeniyi.olaitanhector@yahoo.com>
2023-10-03 04:23:06 +0100

    added media url

--- remote/urls.py
+++ remote/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static  import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -25,3 +26,9 @@ urlpatterns = [
     path('', include('gigs.urls')),
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
